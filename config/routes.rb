4f53063d082210e474fb6cccaa914619c9d2a70b
Olivier Girardot <olive_girardot@hotmail.com>
2021-11-12 11:06:55 +0100

    added bookmark process

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "lists#index"
   resources :movies, only: [:index, :show, :new, :create]
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
