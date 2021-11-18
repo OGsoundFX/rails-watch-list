cdc2a021ddfcbca1d3afc4ecc5268725351a9a90
Olivier Girardot <olive_girardot@hotmail.com>
2021-11-18 20:49:22 +0100

    added pg search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_movies,
+    against: [ :title, :overview, :release_date ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
