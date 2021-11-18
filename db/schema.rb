e7fbe9e4248e7c1825fce004939964dd6e07c1d7
Olivier Girardot <olive_girardot@hotmail.com>
2021-11-18 16:31:48 +0100

    added some sorting buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_141721) do
+ActiveRecord::Schema.define(version: 2021_11_18_150745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_141721) do
     t.decimal "rating", precision: 3, scale: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "release_date"
+    t.integer "popularity"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -66,67 +66,71 @@
 # ------------------------------------------------------------------------------
 # Adding all the movies from the TMDB API
 
-# Bookmark.destroy_all
-# Movie.destroy_all
-# List.destroy_all
-# User.destroy_all
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+User.destroy_all
 
-# puts "Creating Initial User if doesn't exist"
+puts "Creating Initial User if doesn't exist"
 
-# initial_user = User.create(email: "bob@gmail.com", password: '123456', username: 'OGsoundFX')
+initial_user = User.create(email: "bob@gmail.com", password: '123456', username: 'OGsoundFX')
 
-# puts "#{initial_user.username} is present in Database"
+puts "#{initial_user.username} is present in Database"
 
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# base_poster_url = "https://image.tmdb.org/t/p/original"
-# number_of_files = JSON.parse(open(url).read)['total_pages']
+url = "http://tmdb.lewagon.com/movie/top_rated"
+base_poster_url = "https://image.tmdb.org/t/p/original"
+number_of_files = JSON.parse(open(url).read)['total_pages']
 
 
-# JSON.parse(open(url).read)['results'].each do |movie|
-#   Movie.create(
-#     title: movie['title'],
-#     overview: movie['overview'],
-#     poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-#     rating: movie['vote_average']
-#   )
-# end
+JSON.parse(open(url).read)['results'].each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+    rating: movie['vote_average'],
+    release_date: movie['release_date'][0, 4].to_i,
+    popularity: movie['popularity']
+  )
+end
 
-# n = 2
-# (number_of_files.to_i - 1).times do
-#   JSON.parse(open("#{url}?page=#{n}").read)['results'].each do |movie|
-#     Movie.create(
-#       title: movie['title'],
-#       overview: movie['overview'],
-#       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-#       rating: movie['vote_average']
-#     )
-#   end
-#   n += 1
-# end
+n = 2
+(number_of_files.to_i - 1).times do
+  JSON.parse(open("#{url}?page=#{n}").read)['results'].each do |movie|
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average'],
+      release_date: movie['release_date'][0, 4].to_i,
+      popularity: movie['popularity']
+    )
+  end
+  n += 1
+end
 
-# puts "#{Movie.count} have been created"
+puts "#{Movie.count} have been created"
 
-# categories = ['horror', 'fun', 'thriller', 'weekend', 'historic']
+categories = ['horror', 'fun', 'thriller', 'weekend', 'historic']
 
-# categories.each do |cat|
-#   List.create(name: cat, user_id: initial_user.id)
-# end
+categories.each do |cat|
+  List.create(name: cat, user_id: initial_user.id)
+end
 
-# puts "Created #{List.count} lists"
+puts "Created #{List.count} lists"
 
-# 10.times do
-#   Bookmark.create(comment: Faker::Quote.famous_last_words, movie_id: Movie.all.sample.id, list_id: List.all.sample.id)
-# end
+10.times do
+  Bookmark.create(comment: Faker::Quote.famous_last_words, movie_id: Movie.all.sample.id, list_id: List.all.sample.id)
+end
 
-# puts "Created #{Bookmark.count} bookmarks"
+puts "Created #{Bookmark.count} bookmarks"
 
 
 # ------------------------------------------------------------------------------
 # Adding some Video Clubs
 
-VideoClub.destroy_all
+# VideoClub.destroy_all
 
-VideoClub.create(name: "Video Center", address: "Greifswalder Str. 169")
-VideoClub.create(name: "Video Center", address: "Frankfurter Allee 62")
-VideoClub.create(name: "nordberliner-videoclub", address: "Plettenberger Pfad 2A, 13507 Berlin")
-VideoClub.create(name: "Videodrom", address: "Friesenstraße 11, 10965 Berlin")
+# VideoClub.create(name: "Video Center", address: "Greifswalder Str. 169")
+# VideoClub.create(name: "Video Center", address: "Frankfurter Allee 62")
+# VideoClub.create(name: "nordberliner-videoclub", address: "Plettenberger Pfad 2A, 13507 Berlin")
+# VideoClub.create(name: "Videodrom", address: "Friesenstraße 11, 10965 Berlin")
