c195a7fda2d87d71f5efe242383183c87fdb362a
Olivier Girardot <olive_girardot@hotmail.com>
2023-03-11 11:51:44 +0100

    playing around with has_many aliases

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  # has_many :potatoes, class_name: "Bookmark", dependent: :destroy
   has_many :movies, through: :bookmarks
+  # has_many :bananas, through: :bookmarks, source: :movie
   belongs_to :user
   has_one_attached :photo
   
